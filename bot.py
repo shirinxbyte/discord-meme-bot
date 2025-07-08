3258b406fd57455c7591381b9d575335a8a9c87b
Shirin Sharifi <119597062+shirinxbyte@users.noreply.github.com>
2025-07-08 12:49:29 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import requests
+import json
+
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('paste-your-token-here')  # Replace this with your actual token
